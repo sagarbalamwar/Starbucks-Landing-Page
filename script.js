7e779e020034914db2d1b6c2c33e17a108cc34dc
Sagar <sagarbalamwar888@gmail.com>
2022-09-27 10:20:53 +0530

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgslider(anything) {
+  document.querySelector('.starbucks').src = anything
+}
+function changecirclecolor(color) {
+  const circle = document.querySelector('.circle')
+  circle.style.background = color
+}
